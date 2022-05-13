61183d2175254514a6d57295a0c877fbd5298b56
Mike Edwards <mike.edwards@ceprinter.com>
2022-05-13 10:36:16 -0500

    initial commit

---
+++ xml-rpc-client.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+#
+## Purpose: 
+#Usage: 
+
+use strict;
+use warnings;
+use Data::Dumper;
+
+use XMLRPC::Lite;
+
+my $xml	    = '<test><foo>FOO</foo></test>';
+
+my $host    = 'http://services.ceprinter.com:8888';
+#my $host    = 'http://192.168.0.70:8888';
+
+my $server  = XMLRPC::Lite->proxy( $host );
+my $call  = $server->call( 'toMFF', $xml );
+die $call->faultstring if $call->fault;
+print $call->result;
---
+++ xml-rpc-server.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use RPC::XML;
+use RPC::XML::Server;
+use EGA::MidnightFF;
+use Data::Dumper;
+
+
+my $host    = '192.168.0.70';
+#my $host    = 'http://services.ceprinter.com';
+my $port    = 8888;
+
+my $daemon  = RPC::XML::Server->new( host => $host, port => $port );
+
+#$daemon->add_method( {
+$daemon->add_function( {
+	name	    => 'toMFF',
+	#	hidden	    => 1,
+	signature   => [ 'string' ],
+	#code	    => \&Four51toMFF(),
+	code	    => \&test,
+    });
+
+$daemon->server_loop();
+
+sub test {
+    #    my $server_obj  = shift;
+    my $xml	    = shift;
+    print Dumper $xml;
+    Four51toMFF( $xml );
+}
+
